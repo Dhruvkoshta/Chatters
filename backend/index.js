6fcd5b732cd075e8ff5e4c1b8fbb56671f27fbf0
Dhruvkoshta <Dhruvkoshta04@gmail.com>
2024-09-11 16:47:44 +0530

    update env

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,12 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(cors());
+app.use(
+	cors({
+		origin: "*",
+		credentials: true,
+	})
+);
 app.use(express.json());
 app.use(cookieParser());
 
