ae8f4205f72a8e5e1dd90a7fde0ffbb3c82d7364
Dhruvkoshta <Dhruvkoshta04@gmail.com>
2024-09-11 16:41:53 +0530

    update env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	proxy: {
-		"/api": "http://localhost:5000",
-		// change the port accordingly
-	},
 });
